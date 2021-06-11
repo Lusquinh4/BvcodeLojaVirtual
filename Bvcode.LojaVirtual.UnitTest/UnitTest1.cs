09e2026707d8fb58a3554f0a0164941dfe322c2b
Lucas Maxuel <lucas@bvcode.com.br>
2021-06-11 09:21:00 -0300

    Initial Commit

---
+++ Bvcode.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bvcode.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTesteBVcode
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
